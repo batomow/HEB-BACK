131588bb575a12120226dbdbf7f0d59c1c936919
batomow <carlosa_miranda@outlook.com>
2020-05-24 16:35:11 -0700

    adds get current calendar

--- routes/calendar.js
+++ routes/calendar.js
@@ -1,33 +1,26 @@
 const router = require('express').Router()
 
-// function getDate(){
-//     let date = new Date(); 
-
-//     let day = date.getDate()
-//     day = (day < 10 ? "0" : "") + day
-
-//     let month = date.getMonth()
-//     month = (month < 10 ? "0" : "") + month
-
-//     let year = date.getFullYear()
-
-//     return {day, month, year}
-// }
-
-const DAYS = ['friday', 'monday', 'saturday', 'sunday', 'thursday', 'tuesday', 'wednesday']
-router.get('/:user', async (req, res)=>{
-    const fecha = "4-5-2020"
-    const user = req.params.user
+const DAYS = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+router.post('/all', async (req, res)=>{
+    const {user, fecha } = req.body
     const firestore = req.firestore
     const monolito = {} 
     let snapshot = await firestore.collection('user').doc(user).get()
     if (!snapshot.exists){
-        return res.status(404).send("no hay usuario con ese id")
+        return res.send({
+            error: true, 
+            message: "no hay usuario con ese id", 
+            data: ""
+        })
     }
     monolito.user = snapshot.data()
     snapshot = await firestore.collection('user').doc(user).collection('week').doc(fecha).get()
     if (!snapshot.exists){
-        return res.status(404).send("no hay calendario para esa fecha")
+        return res.send({
+            error: true,
+            message: "no hay calendario para esa fecha",
+            data: ""
+        })
     }
     monolito.resume = snapshot.data()
     const promises = [] 
@@ -35,57 +28,14 @@ router.get('/:user', async (req, res)=>{
         promises.push(firestore.collection('user').doc(user).collection('week').doc(fecha).collection('days').doc(day).get())
     }
     const snapshots = await Promise.all(promises)
-    monolito[fecha] = {}
+    monolito.current = []
     for (const index in DAYS){
-        monolito[fecha][DAYS[index]] = snapshots[index].data()
+        monolito.current.push(snapshots[index].data())
     }
-    res.status(501).send({
-        message: "OK!", 
+    res.send({
+        error: false,
+        message: "Bienvenido!", 
         data: {...monolito}
-    })
-})
-
-
-router.get('/:user/past', (req, res)=>{ // this calendar is relative to the "current calendar"
-    // TODO
-    res.status(501).send({
-        message: "TODO: GET PAST WEEK CALENDAR",
-        data: {
-            user: req.params.user
-        }
-    })
-})
-
-router.get('/:user/future', (req, res)=>{ // this calendar is relative to the "current calendar" 
-    // TODO
-    res.status(501).send({
-        message: "TODO: GET NEXT CALENDAR",
-        data: {
-            user: req.params.user
-        }
-    })
-})
-
-router.get('/:user/:day', (req, res)=>{
-    // TODO
-    res.status(501).send({
-        message: "TODO: GET DAY RESUME", 
-        data: {
-            user: req.params.user,
-            day: req.params.day
-        }
-    })
-})
-
-router.get("/:user/:day/:field", (req, res)=>{
-    // TODO
-    res.status(501).send({
-        message: "TODO: GET A FIELD FOR A PARTICULAR DAY", 
-        data: {
-            user: req.params.user,
-            day: req.params.day, 
-            field: req.params.field
-        }
     })
 })
 
--- routes/login.js
+++ routes/login.js
@@ -1,15 +1,35 @@
 const router = require('express').Router()
 
 router.post('/', async (req, res)=>{
+    const {fecha, user, password} = req.body
+    if (!fecha || !user || !password){
+        return res.send({
+            error: true, 
+            message: "Faltan llenar datos", 
+            data: ""
+        })
+    }
     const firestore = req.firestore
     const snapshot = await firestore.collection('user').doc(req.body.user).get()
-    if (!snapshot){
-        return res.status(401).send(`There is no user with ${req.body.user} id`)
+    if (!snapshot.exists){
+        return res.send({
+            error: true, 
+            message: 'No hay usuario con ese id', 
+            data: ""
+        })
     }
     if (req.body.password != snapshot.data().password){
-        return res.status(401).send("Wrong password")
+        return res.send({
+            error: true, 
+            message: "contraseña incorrecta", 
+            data: ""
+        })
     }
-    res.status(200).send("Bienvenido")
+    res.send({
+        error: false, 
+        message: "Bienvenido",
+        data: ""
+    })
 })
 
 module.exports = router
